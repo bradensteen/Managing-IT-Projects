d4e08b2e30784259b5b0f31689ee77897f896f97
bradensteen <102957658@student.swin.edu.au>
2022-05-05 18:47:18 +1000

    commit

---
+++ customerinvestments.php
@@ -0,0 +1,31 @@
+<?php
+include "includes/dbcon.php";
+
+// Check user login or not
+if(!isset($_SESSION['uname'])){
+    header('Location: customerlogin.php');
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Add a Customer</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    <div class="login_container">
+    <div id="div_home">
+    <h1>Add a Customer</h1>
+        <div class="topnav">
+        <a class="active" href="home.php">Home</a>
+        <a href="customerinvestments.php">View Investments</a>
+        </div> 
+    <p>Coming Soon</p>    
+</div>
+</div>
+</body>
+</html>
+
